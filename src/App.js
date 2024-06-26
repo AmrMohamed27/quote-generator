cd3ad0e6a0d8ff519d0e4bf90b9964f752240904
AmrMohamed27 <amrmohamedorigi44@gmail.com>
2024-06-27 02:46:51 +0300

    Add Social Media Buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import { FaXTwitter as TwitterIcon } from "react-icons/fa6";
+import { FaWhatsapp as WhatsappIcon } from "react-icons/fa6";
+import { FaTelegram as TelegramIcon } from "react-icons/fa6";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const twitterURL = "https://twitter.com/intent/tweet?text=";
+  const whatsappURL = "https://api.whatsapp.com/send?text=";
+  const telegramURL = "https://t.me/share/url?url=Quote:&text=";
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +38,53 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+        <div className="share-container">
+          <div className="share">
+            <TwitterIcon className="icon" />
+            <a
+              className=""
+              href={encodeURI(
+                `${twitterURL}${quote.author} once said: ${quote.content}`
+              )}
+            >
+              Share to X
+            </a>
+          </div>
+          <div className="share">
+            <WhatsappIcon className="icon" />
+            <a
+              className=""
+              href={encodeURI(
+                `${whatsappURL}${quote.author} once said: ${quote.content}`
+              )}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send in WhatsApp
+            </a>
+          </div>
+          <div className="share">
+            <TelegramIcon className="icon" />
+            <a
+              className=""
+              href={encodeURI(
+                `${telegramURL}${quote.author} once said: ${quote.content}`
+              )}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send via Telegram
+            </a>
+          </div>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
